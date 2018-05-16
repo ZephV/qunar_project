03dc3220bc1eb7731562d493f52e38ff48a7cf3f
ZephV <zephkwok@outlook.com>
2018-05-16 17:46:11 +0800

    animation finish

--- qunar/src/pages/detail/components/Banner.vue
+++ qunar/src/pages/detail/components/Banner.vue
@@ -10,16 +10,20 @@
         </div>
       </div>
     </div>
-    <common-gallary :imgs="gallaryImgs" v-show="showGallary" @close="handleGallaryClose"></common-gallary>
+    <fade-animation>
+      <common-gallary :imgs="gallaryImgs" v-show="showGallary" @close="handleGallaryClose"></common-gallary>
+    </fade-animation>
   </div>
 </template>
 
 <script>
 import CommonGallary from 'common/gallary/Gallary'
+import FadeAnimation from 'common/fade/FadeAnimation'
 export default {
   name: 'Banner',
   components: {
-    CommonGallary
+    CommonGallary,
+    FadeAnimation
   },
   props: {
     bannerImg: String,
