9b071e474c18f8921427dfc989ee0a530323baca
ZephV <zephkwok@outlook.com>
2018-05-16 17:30:23 +0800

    ajx

--- qunar/src/pages/detail/components/Banner.vue
+++ qunar/src/pages/detail/components/Banner.vue
@@ -1,16 +1,16 @@
 <template>
   <div>
     <div class="banner" @click="handleGallaryClick">
-      <img class="banner-img" src="http://img1.qunarzz.com/sight/p0/201404/23/04b92c99462687fa1ba45c1b5ba4ad77.jpg_600x330_bf9c4904.jpg">
+      <img class="banner-img" :src="bannerImg">
       <div class="banner-info">
-        <div class="banner-title">大连圣亚海洋世界(AAAA景区)</div>
+        <div class="banner-title">{{this.sightName}}</div>
         <div class="banner-number">
           <span class="iconfont banner-icon">&#xe692;</span>
-          39
+          {{this.gallaryImgs.length}}
         </div>
       </div>
     </div>
-    <common-gallary :imgs="imgs" v-show="showGallary" @close="handleGallaryClose"></common-gallary>
+    <common-gallary :imgs="gallaryImgs" v-show="showGallary" @close="handleGallaryClose"></common-gallary>
   </div>
 </template>
 
@@ -21,9 +21,13 @@ export default {
   components: {
     CommonGallary
   },
+  props: {
+    bannerImg: String,
+    sightName: String,
+    gallaryImgs: Array
+  },
   data () {
     return {
-      imgs: ['http://img1.qunarzz.com/sight/p0/1412/df/b8e2bca53e9f51a9a8a8b50e02c2746f.water.jpg_r_800x800_21d8630d.jpg', 'http://img1.qunarzz.com/sight/p0/1412/df/b8e2bca53e9f51a9a8a8b50e02c2746f.water.jpg_r_800x800_21d8630d.jpg'],
       showGallary: false
     }
   },
